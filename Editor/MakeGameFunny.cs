5edd6125cf70c5f55950774d7fd4d3299d72a6da
rainbowshyy <simskje@gmail.com>
2024-01-26 21:30:59 +0100

    Initial commit

---
+++ Editor/MakeGameFunny.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Simen.FunnyPackage.Editor
+{
+    public class MakeGameFunny : MonoBehaviour
+    {
+        [MenuItem("Funny/Make game funny")]
+        public static void FunnyGame()
+        {
+
+        }
+    }
+}
